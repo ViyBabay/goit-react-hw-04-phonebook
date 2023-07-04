15a3b0b6323f3c7a343d7c3dc90ce2aaba359096
Александр Пикуш <pikush.oleksandr@gmail.com>
2023-07-04 19:30:13 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,11 +28,10 @@ export const App = () => {
     setFilter(e.target.value);
   };
 
-  const getVisibleContacts = () => {
-    return contacts.filter(contact =>
+  const getVisibleContacts = () =>
+    contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
-  };
 
   const onAddContact = ({ name, number }) => {
     if (
@@ -44,20 +43,6 @@ export const App = () => {
     const newContact = { id: crypto.randomUUID(), name, number };
     setContacts(prev => [...prev, newContact]);
   };
-
-  //   componentDidMount() {
-  //     const contacts = localStorage.getItem('contacts');
-  //     const parsedContacts = JSON.parse(contacts);
-  //     if (parsedContacts) {
-  //       this.setState({ contacts: parsedContacts });
-  //     }
-  //   }
-
-  //   componentDidUpdate(prevProps, prevState) {
-  //     if (this.state.contacts !== prevState.contacts) {
-  //       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  //     }
-  //   }
 
   const visibleContacts = getVisibleContacts();
   return (
@@ -78,88 +63,3 @@ export const App = () => {
     </div>
   );
 };
-
-// export class App extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-//     filter: '',
-//     name: '',
-//   };
-
-// deleteContact = contactId => {
-//   this.setState(prevState => ({
-//     contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-//   }));
-// };
-
-//   filterContact = evt => {
-//     this.setState({ filter: evt.currentTarget.value });
-//   };
-
-//   getVisibleContacts = () => {
-//     const { contacts, filter } = this.state;
-// const normilizedFilter = filter.toLowerCase();
-
-// return contacts.filter(contact =>
-//   contact.name.toLowerCase().includes(normilizedFilter)
-// );
-//   };
-
-//   onAddContact = contactItem => {
-//     if (
-//       this.state.contacts.filter(
-//         elem => elem.name.toLowerCase() === contactItem.name.toLowerCase()
-//       ).length > 0
-//     ) {
-//       return alert(`${contactItem.name} is already in contacts`);
-//     }
-//     this.setState(prevState => {
-//       const arr = [...prevState.contacts, contactItem];
-//       return { contacts: arr };
-//     });
-//   };
-
-//   componentDidMount() {
-//     const contacts = localStorage.getItem('contacts');
-//     const parsedContacts = JSON.parse(contacts);
-//     if (parsedContacts) {
-//       this.setState({ contacts: parsedContacts });
-//     }
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     if (this.state.contacts !== prevState.contacts) {
-//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-//     }
-//   }
-
-//   render() {
-//     const { filter } = this.state;
-//     const visibleContacts = this.getVisibleContacts();
-//     return (
-//       <div
-//         style={{
-//           display: 'flex',
-//           flexDirection: 'column',
-//           justifyContent: 'center',
-//           alignItems: 'center',
-//         }}
-//       >
-//         <h1>PhoneBook</h1>
-//         <ContactForm onAddContact={this.onAddContact} />
-
-//         <h2>Contacts</h2>
-//         <Filter value={filter} onChange={this.filterContact} />
-//         <ContactList
-//           contacts={visibleContacts}
-//           onDeleteContact={this.deleteContact}
-//         />
-//       </div>
-//     );
-//   }
-// }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -30,7 +30,7 @@ export const ContactForm = ({ onAddContact }) => {
           className={s.input}
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           value={name}
           onChange={handleChange}
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
